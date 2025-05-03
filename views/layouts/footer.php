a62c496b77ed0d4c2abf043e65716834e3196cb2
OneServiceJL <oneservice_2025@outlook.com>
2025-05-02 22:39:32 -0700

    style file

--- views/layouts/footer.php
+++ views/layouts/footer.php
@@ -1,13 +1,14 @@
 <footer class="text-muted py-5">
     <div class="container">
-        <p class="float-right mb-1">
+        <p class="float-end mb-1">
             <a href="#">Back to top</a>
         </p>
-        <p class="mb-1">Album example is &copy; Bootstrap, but please download and customize it for yourself!</p>
-        <p class="mb-0">New to Bootstrap? <a href="/">Visit the homepage</a> or read our <a
-                href="/docs/5.0/getting-started/introduction/">getting started guide</a>.</p>
+        <p class="mb-1">CRM Project &copy; 2023</p>
+        <p class="mb-0">New to Bootstrap? <a href="https://getbootstrap.com/docs/5.0/getting-started/introduction/">Visit the homepage</a> or read our <a href="https://getbootstrap.com/docs/5.0/getting-started/download/">getting started guide</a>.</p>
     </div>
 </footer>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="assets/js/bootstrap.min.js"></script>
 </body>
 
